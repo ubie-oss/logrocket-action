4bacaa2df62070f33a084cde70db9f24b07e3e85
Yuku Kotani <yukukotani@gmail.com>
2022-02-03 23:14:51 +0900

    consume input

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // const apiKey = core.getInput("api-key");
+    const release = core.getInput("release");
+    // const sourceMapDir = core.getInput("source-map-dir");
+    // const urlPrefix = core.getInput("url-prefix");
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput("release", release);
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
